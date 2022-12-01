04121815edf196586d26402f4e4fe9b52bea142e
Heltev <vetle.fredriksen@gmail.com>
2022-12-01 22:15:38 +0100

    Add input parameters, handle corner case

--- 1/day1.py
+++ 1/day1.py
@@ -1,16 +1,21 @@
+import sys
+
 def main():
   tot_cals = 0
   max_cals = 0
   all_cals = []
-  for line in open('input.txt'):
+  for line in open(str(sys.argv[1])+'.txt'):
     if line == '\n':
         if tot_cals > max_cals:
             max_cals = tot_cals
         all_cals.append(tot_cals)
-        all_cals.sort()
         tot_cals = 0
     else:
       tot_cals += int(line)
+  if tot_cals > max_cals:
+    max_cals = tot_cals
+  all_cals.append(tot_cals)
+  all_cals.sort()
   print (max_cals)
   print(all_cals[-1]+all_cals[-2]+all_cals[-3])
 
